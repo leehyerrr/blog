d50c6d93fc917282cfb7d29b420c2531fab6c072
leehyerrr <leehyerrr@gmail.com>
2022-08-16 15:58:21 +0900

    22-08-16 blog첫작업

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>  
+</template>
+
+<script>
+export default {
+    name: 'List'
+}
+</script>
+
+<style>
+
+</style>
